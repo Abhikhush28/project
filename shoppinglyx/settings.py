75aaf2c77ded091407ecdbf1851793bcd9c33ad4
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-03-28 00:30:44 +0530

    Update settings.py

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qnnpk94mjoiz^whadhlmtlxq7+-j@#7ndmf86k0-43$7r%gyz5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -128,3 +128,6 @@ SESSION_ENGINE = "django.contrib.sessions.backends.db"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
